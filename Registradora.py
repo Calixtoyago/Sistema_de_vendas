012cb240bded92ca34b395d756fba93ebb59330a
Yago Calixto <yagocalixto020@gmail.com>
2025-05-02 00:48:21 -0300

    feat: Adicionado funcoes no menu

--- Registradora.py
+++ Registradora.py
@@ -7,10 +7,15 @@ class Registradora:
         self.connector = sqlite3.connect(banco)
         self.cursor = self.connector.cursor()
 
-    def mostrarTabelasCompletas(self, tabela):
+    def mostrarTabelasCompletas(self, tabela, show=True):
         query = f"SELECT * FROM {tabela}"
-        db = pd.read_sql(query, self.connector)
-        print(tabulate(db, headers='keys', tablefmt='fancy_grid'))
+        if show:
+            db = pd.read_sql(query, self.connector)
+            # if tabela == "produtos":
+                # db["valor_unitario"] = db["valor_unitario"].astype(float).round(2)
+            print(tabulate(db, headers='keys', tablefmt='fancy_grid', showindex=False))
+        else:
+            return self.cursor.execute(query).fetchall()
 
     def mostrarProdutosVendidos(self):
         query = """SELECT 
@@ -23,7 +28,7 @@ class Registradora:
                     JOIN produtos p ON pv.produto_id = p.codigo_produto
                     JOIN vendas v ON pv.venda_id = v.codigo_venda"""
         db = pd.read_sql(query, self.connector)
-        print(tabulate(db, headers='keys', tablefmt='fancy_grid'))
+        print(tabulate(db, headers='keys', tablefmt='fancy_grid', showindex=False))
 
     def mostrarVendas(self):
         query = """
@@ -40,9 +45,9 @@ class Registradora:
                 GROUP BY v.codigo_venda
                 """
         db = pd.read_sql(query, self.connector)
-        db["valor_total"] = db["valor_total"].map('{:.2f}'.format)
+        # db["valor_total"] = db["valor_total"].astype(float).round(2)
         db = db.rename(columns={'valor_total' : 'valor_total (R$)'})
-        print(tabulate(db, headers='keys', tablefmt='fancy_grid'))
+        print(tabulate(db, headers='keys', tablefmt='fancy_grid', showindex=False))
 
     def mostrarUnicaVenda(self, venda_id):
         query = """
@@ -60,9 +65,9 @@ class Registradora:
                 GROUP BY v.codigo_venda
                 """
         db = pd.read_sql(query, self.connector, params=(venda_id, ))
-        db["valor_total"] = db["valor_total"].map('{:.2f}'.format)
+        # db["valor_total"] = db["valor_total"].astype(float).round(2)
         db = db.rename(columns={'valor_total' : 'valor_total (R$)'})
-        print(tabulate(db, headers='keys', tablefmt='fancy_grid'))
+        print(tabulate(db, headers='keys', tablefmt='fancy_grid', showindex=False))
         # print(db.to_string(index=False))
     
 class Produtos(Registradora):
@@ -82,6 +87,11 @@ class Produtos(Registradora):
             print("Produto cadastrado")
         
             self.mostrarTabelasCompletas("produtos")
+    
+    def verProdutoUnico(self, nome):
+        query = "SELECT * FROM produtos WHERE nome = ?"
+        db = pd.read_sql(query, self.connector, params=(nome,))
+        print(tabulate(db, headers='keys', tablefmt='fancy_grid', showindex=False))
 
 class Vendas(Registradora):
     def __init__(self, banco="registros.db"):
@@ -101,6 +111,7 @@ class Vendas(Registradora):
             for item in lista:
                 produto_id, quantidade = item
                 valor_unitario = self.cursor.execute("SELECT valor_unitario FROM produtos WHERE codigo_produto = ?", (produto_id,)).fetchone()[0]
+                valor_unitario = float(f"{valor_unitario:.2f}")
                 self.cursor.execute("""
                     INSERT INTO produtos_vendidos (venda_id, produto_id, quantidade, valor_total)
                     VALUES (?, ?, ?, ?)
@@ -130,53 +141,133 @@ class Vendas(Registradora):
             WHERE codigo_venda = ?
         """, (quantidade_total_venda, valor_total_venda, venda_id))
 
+def menu():
+    ADMIN = "12345"
+    registrador = Registradora()
+    vendas = Vendas()
+    produtos = Produtos()
+
+    opcao = ""
+    while opcao != 0:
+        print("""
+[1] Registrar produto
+[2] Registrar venda
+[3] Ver vendas
+[4] Ver produtos
+[0] Sair
+    """)
+        while True:
+            opcao = input(">>>> ")
+            if opcao in ('1', '2', '3', '4'):
+                break
+            else:
+                print("Opcao invalida")
+
+        match opcao:
+            case '1':
+                try:
+                    nome = input("Nome do produto: ")
+                    valor = float(input("Valor unitario: R$"))
+                    estoque = int(input("Quantidade no estoque: "))
+
+                    valor = float(f"{valor:.2f}")
+
+                except TypeError:
+                    print("Valor inserido invalido - registro cancelado")
+                else:
+                    produtos.cadastrarProdutos(nome, valor, estoque)
+                    print("Produto cadastrado com sucesso")
+                    produtos.verProdutoUnico(nome)
+            case '2':
+                adicionar = ""
+                lista_produtos = []
+                while adicionar != "nao":
+                    registrador.mostrarTabelasCompletas("produtos")
+                    
+                    try:
+                        codigo = int(input("Codigo do produto: "))
+                        quantidade = int(input("Quantidade: "))
+                    except ValueError:
+                        print("ERROR - Valor invalido. Compra cancelada")
+                    else:
+                        quantidade_produto = registrador.cursor.execute("SELECT estoque FROM produtos WHERE codigo_produto = ?", (codigo, )).fetchone()[0]
+                        if quantidade > quantidade_produto:
+                            print("ERROR - Quantidade ultrapassa o estoque do produto")
+                            continue
+                        lista_produtos.append((codigo, quantidade))
+                    
+                    while True:
+                        adicionar = input("Adicionar outro produto: [Sim/Nao] >> ").lower().strip()
+                        if adicionar not in ("sim", "nao"):
+                            print("ERROR - Opcao invalida")
+                        else: 
+                            break
+                
+                # codigos = []
+                # for produto in lista_produtos:
+                #     codigos.append(produto[0])
+
+                # placeholders = ','.join(['?'] * len(codigos))  # Resultado: '?,?,?'
+                # query = f"SELECT * FROM produtos WHERE codigo_produto IN ({placeholders})"
+                # db = pd.read_sql(query, registrador.connector, params=codigos)
+                # print(tabulate(db, headers='keys', tablefmt='fancy_grid', showindex=False))
+
+                # registrador.cursor.execute()
+                vendas.registrarVendas(lista_produtos)
+
+                   
+
+                
+            case '3':
+                registrador.mostrarVendas()
+
+    
+menu()
+# registrador = Registradora()
+# vendas = Vendas()
+# produtos = Produtos()
+
+# # produtos.cadastrarProdutos("Borracha", 1, 10)
+# # produtos.cadastrarProdutos("Lápis", 0.5, 30)
+# # produtos.cadastrarProdutos("Caderno", 10, 10)
+# # produtos.cadastrarProdutos("Estojo", 15, 15)
+# # produtos.cadastrarProdutos("Arroz", 20.00, 50)
+# # produtos.cadastrarProdutos("Feijão", 8.50, 40)
+# # produtos.cadastrarProdutos("Macarrão", 5.75, 60)
+# # produtos.cadastrarProdutos("Sabonete", 2.99, 100)
+# # produtos.cadastrarProdutos("Detergente", 3.50, 80)
+# # produtos.cadastrarProdutos("Caderno", 12.00, 30)
+# # produtos.cadastrarProdutos("Lápis", 1.50, 200)
+# # produtos.cadastrarProdutos("Caneta", 2.00, 150)
+# # produtos.cadastrarProdutos("Borracha", 0.75, 120)
+# # produtos.cadastrarProdutos("Leite", 6.90, 35)
+
+# # registrador.mostrarTabelasCompletas("produtos")
+
+# # lista1 = [(1, 2), (4, 1), (10, 5)]
+
+# # vendas.registrarVendas(lista1)
+
+# # lista2 = [(5, 5), (6, 10), (9, 2)]
+
+# # vendas.registrarVendas(lista2)
+
+# print()
 
 
-registrador = Registradora()
-vendas = Vendas()
-produtos = Produtos()
-
-# produtos.cadastrarProdutos("Borracha", 1, 10)
-# produtos.cadastrarProdutos("Lápis", 0.5, 30)
-# produtos.cadastrarProdutos("Caderno", 10, 10)
-# produtos.cadastrarProdutos("Estojo", 15, 15)
-# produtos.cadastrarProdutos("Arroz", 20.00, 50)
-# produtos.cadastrarProdutos("Feijão", 8.50, 40)
-# produtos.cadastrarProdutos("Macarrão", 5.75, 60)
-# produtos.cadastrarProdutos("Sabonete", 2.99, 100)
-# produtos.cadastrarProdutos("Detergente", 3.50, 80)
-# produtos.cadastrarProdutos("Caderno", 12.00, 30)
-# produtos.cadastrarProdutos("Lápis", 1.50, 200)
-# produtos.cadastrarProdutos("Caneta", 2.00, 150)
-# produtos.cadastrarProdutos("Borracha", 0.75, 120)
-# produtos.cadastrarProdutos("Leite", 6.90, 35)
+# registrador.mostrarProdutosVendidos()
+# registrador.mostrarVendas()
 
 # registrador.mostrarTabelasCompletas("produtos")
 
-# lista1 = [(1, 2), (4, 1), (10, 5)]
+# lista3 = [(1, 10)]
 
-# vendas.registrarVendas(lista1)
+# vendas.registrarVendas(lista3)
 
-# lista2 = [(5, 5), (6, 10), (9, 2)]
+# lista4 = [(7, 30), (5, 10)]
 
-# vendas.registrarVendas(lista2)
+# vendas.registrarVendas(lista4)
 
-print()
-
-
-registrador.mostrarProdutosVendidos()
-registrador.mostrarVendas()
-
-registrador.mostrarTabelasCompletas("produtos")
-
-lista3 = [(1, 10)]
-
-vendas.registrarVendas(lista3)
-
-lista4 = [(7, 30), (5, 10)]
-
-vendas.registrarVendas(lista4)
-
-# registrador.mostrarProdutosVendidos()
-registrador.mostrarVendas()
-registrador.mostrarTabelasCompletas("produtos")
+# # registrador.mostrarProdutosVendidos()
+# registrador.mostrarVendas()
+# registrador.mostrarTabelasCompletas("produtos")
